04b9985e2605ca9d7ba95f679c69a21624731a40
Guest User <Guest@Epicodus-10G.local>
2017-08-11 10:45:52 -0700

    add front end versus back end selector and add input into quote generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,24 @@
 $(document).ready(function() {
   $("form#frontEndBackEnd").submit(function(event) {
     var nameInput = $("input#name").val();
-    // var designVsLogic = $("select#designVsLogic").val();
-    // var userExperience = $("select#userExperience").val();
-    // var backEnd = "Back End";
-    // var frontEnd = "Front End";
+    var designVsLogic = $("select#designVsLogic").val();
+    var frontEnd = "Front End";
+    var backEnd = "Back End";
+
+    if (designVsLogic) {
+      if (designVsLogic === "design"){
+
+      $(".dev").empty().prepend(frontEnd);
+      }
+      else {
+        $(".dev").empty().prepend(backEnd);
+      }
+    }
+
 
     $(".name").text(nameInput);
     $("#quote").show();
 
-
-    // if (designVsLogic) {
-    //   if (designVsLogic === 'Design' && userExperience === 'I want to make the internet beautiful, fun, and easy to use!') {
-    //
-    //     $("#name").text(nameInput);
-    //     $('#dev').empty().prepend(frontEnd);
-    //     $('.quote').show();
-    //   }
-    //   else (){
-    //
-    //     $('#name').text(nameInput);
-    //     $('#dev').empty().prepend(backEnd);
-    //     $('.quote').show();
-    //   }
-    // }
     event.preventDefault();
   });
 });
