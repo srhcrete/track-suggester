becedd85c496e3ef78bc3cfb4afa8986fa456669
Guest User <Guest@Epicodus-10G.local>
2017-08-11 09:28:53 -0700

    set initial html, styles, and scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+  $("form#frontEndBackEnd").submit(function(event) {
+    var name = $("input#name").val();
+
+    $('.name').text(devInput);
+
+    // $('#quote').show();
+
+
+  });
+});
