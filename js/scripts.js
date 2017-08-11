b72971f5f0d6bb9910dd50c7da6daf9c160cb354
Guest User <Guest@Epicodus-10G.local>
2017-08-11 10:13:07 -0700

    fix name input into quote generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
-$(function() {
+$(document).ready(function() {
   $("form#frontEndBackEnd").submit(function(event) {
-    var name = $("input#name").val();
+    var nameInput = $("input#name").val();
+    // var designVsLogic = $("select#designVsLogic").val();
+    // var userExperience = $("select#userExperience").val();
+    // var backEnd = "Back End";
+    // var frontEnd = "Front End";
 
-    $('.name').text(devInput);
+    $(".name").text(nameInput);
+    $("#quote").show();
 
-    // $('#quote').show();
 
-
+    // if (designVsLogic) {
+    //   if (designVsLogic === 'Design' && userExperience === 'I want to make the internet beautiful, fun, and easy to use!') {
+    //
+    //     $("#name").text(nameInput);
+    //     $('#dev').empty().prepend(frontEnd);
+    //     $('.quote').show();
+    //   }
+    //   else (){
+    //
+    //     $('#name').text(nameInput);
+    //     $('#dev').empty().prepend(backEnd);
+    //     $('.quote').show();
+    //   }
+    // }
+    event.preventDefault();
   });
 });
